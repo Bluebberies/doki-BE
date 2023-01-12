7444616a87dc82f4aacff51897ecef73f8f72a07
Francis <francisalexander000@gmail.com>
2023-01-13 00:03:29 +0100

    modified port

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'
 import helmet from 'helmet';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet())
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
   // console.log(helmet())
 }
 bootstrap();
